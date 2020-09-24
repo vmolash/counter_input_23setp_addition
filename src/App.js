1fa7761f9d8cd6573be20355b28548154ae58611
vmolash <molash.v@gmail.com>
2020-09-23 18:21:04 -0700

    Added counter with input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import Controller from "./Controller";
+import List from "./List";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [list, setList] = useState([
+        {value: 0, id: 1, numberOfButtons: [1,2,3]},
+        {value: 1, id: 2, numberOfButtons: [1,2,3]},
+        {value: 2, id: 3, numberOfButtons: [1,2,3]}
+    ]);
+    const [inputValue, setInputValue] = useState('');
+    const [number, setNumber] = useState('');
+
+    const addCounter = () => {
+        const newButtons = [];
+        for (let i = 1; i <= number ; i++) {
+            newButtons.push(i);
+        }
+        const newCounter = {
+            value: inputValue,
+            id: Math.random(),
+            numberOfButtons: newButtons
+        }
+        const newList = [...list, newCounter];
+        // newList.push({value: 0, id: Math.random()});
+        setList(newList);
+        setInputValue('');
+        setNumber('');
+    }
+    const inputHandlerValue = (e) => {
+        setInputValue(e.target.value);
+    }
+    const inputHandlerNumber = (e) => {
+        setNumber(e.target.value);
+    }
+    const changeCounter = (id, value) => {
+        const newList = list.map(el => {
+            if (el.id === id) {
+                // return {value: el.value + 1, id: el.id}
+                return {...el, value: el.value+value}
+            }
+            return el
+        })
+        setList(newList);
+    }
+    return (
+        <div>
+           <Controller addCounter={addCounter} inputHandlerValue={inputHandlerValue} inputHandlerNumber={inputHandlerNumber} inputValue={inputValue} number={number}/>
+           <List list={list} changeCounter={changeCounter}/>
+        </div>
+    );
 }
 
 export default App;
